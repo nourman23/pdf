13255dee81e2814c1b0e2b93d3826a816ed15689
nourman23 <nourmanzawahreh@gmail.com>
2023-02-08 17:18:22 +0300

    pdf

---
+++ codeigniter/application/views/GeneratePdfView.php
@@ -0,0 +1,509 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta charset="utf-8">
+    <title>Create PDF from View in CodeIgniter Example</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+</head>
+<style>
+
+    table{
+        height:900px;
+        margin-top:50px;
+        margin-bottom:50px;
+    }
+    @page{
+                margin-top: 150px;
+                margin-bottom: 150px;
+            }
+            header{
+                position: fixed;
+                left: 0px;
+                right: 0px;
+                height: 150px;
+                margin-top: -150px;
+            }
+            footer{
+                position: fixed;
+                left: 0px;
+                right: 0px;
+                height: 150px;
+                bottom: 0px;
+                margin-bottom: -150px;
+            }
+</style>
+<body>
+    <header><h1>header</h1> </header>
+<h1 class="text-center bg-info">Generate PDF from View using DomPDF</h1>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<table class="table table-striped table-hover">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Book Name</th>
+            <th>Author</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>1</td>
+            <td>PHP and MySQL for Dynamic Web Sites</td>
+            <td>Larry Ullman</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Pro MEAN Stack Development</td>
+            <td>Elad Elrom</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Restful API Design</td>
+            <td>Matthias Biehl</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Pro PHP MVC</td>
+            <td>Chris Pitt</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>Mastering Spring MVC 4</td>
+            <td>Geoffroy Warin</td>
+        </tr>
+        <tbody>
+</table>
+<!-- <footer><h1>footer</h1> </footer> -->
+</body>
+</html>
